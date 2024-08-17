c99ba9bc3a0e22511e9b36b8f351b19363f11e5b
BinahDaath <79816877+BinahDaath@users.noreply.github.com>
2024-08-17 00:52:05 +0000

    test

---
+++ src/services/createGame.js
@@ -0,0 +1,21 @@
+"use server";
+
+import { createClient } from "@/utils/supabase/server";
+import { revalidatePath } from "next/cache";
+
+export const createPerson = async (personData) => {
+  const supabase = createClient("https://tuhjrjpmjlelzyiqvckc.supabase.co","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR1aGpyanBtamxlbHp5aXF2Y2tjIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyMTY5MTg4MiwiZXhwIjoyMDM3MjY3ODgyfQ.8hyNUQwQLTekqZPfuJSTew3c3HnC-RuYBfzYc3cHaLA");
+  console.log(supabase);
+  const { data, error } = await supabase
+    .from("todos")
+    .insert([personData])
+    .select();
+
+  revalidatePath("/");
+
+  if (data.length > 0) {
+    return data[0];
+  }
+
+  return null;
+};
