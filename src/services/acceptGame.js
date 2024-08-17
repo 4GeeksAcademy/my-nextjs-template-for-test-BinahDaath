f167152cff5d4b78f84c5be626dbd97726626524
BinahDaath <79816877+BinahDaath@users.noreply.github.com>
2024-08-17 03:14:40 +0000

    test

--- src/services/acceptGame.js
+++ src/services/acceptGame.js
@@ -4,6 +4,7 @@
 import { createClient } from '@supabase/supabase-js'
 import { revalidatePath } from "next/cache";
 import { getUser } from "@/services/getUser";
+import { ChessTable } from '@/components/Chesstable';
 export const acceptGame = async (id) => {
   //const supabase = createClient();
   const supabase = createClient("https://tuhjrjpmjlelzyiqvckc.supabase.co","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR1aGpyanBtamxlbHp5aXF2Y2tjIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyMTY5MTg4MiwiZXhwIjoyMDM3MjY3ODgyfQ.8hyNUQwQLTekqZPfuJSTew3c3HnC-RuYBfzYc3cHaLA");
@@ -11,8 +12,16 @@ export const acceptGame = async (id) => {
   const user=await getUser();
   const { data, error } = await supabase
     .from("game_proposition")
-    .update([{status:"accepted"}])
-    .eq("id",id);
+    .update({status:"accepted"})
+    .eq("id",id)
+    .select();
+    if(data.color==="black")
+    {
+      supabase
+      .from("game")
+      .insert([{player1:data.player,player2:user.id,status:"accepted",time,time1,time2,chesstable}])
+    }
+
   console.log(data);
   console.log(error);
   revalidatePath("/");
