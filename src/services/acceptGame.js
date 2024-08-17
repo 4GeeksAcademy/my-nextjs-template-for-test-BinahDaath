800f529d29c9111a1e127073a7574e713f349d27
BinahDaath <79816877+BinahDaath@users.noreply.github.com>
2024-08-17 02:57:17 +0000

    test

--- src/services/acceptGame.js
+++ src/services/acceptGame.js
@@ -4,15 +4,15 @@
 import { createClient } from '@supabase/supabase-js'
 import { revalidatePath } from "next/cache";
 import { getUser } from "@/services/getUser";
-export const acceptGame = async (gameData) => {
+export const acceptGame = async (id) => {
   //const supabase = createClient();
   const supabase = createClient("https://tuhjrjpmjlelzyiqvckc.supabase.co","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR1aGpyanBtamxlbHp5aXF2Y2tjIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyMTY5MTg4MiwiZXhwIjoyMDM3MjY3ODgyfQ.8hyNUQwQLTekqZPfuJSTew3c3HnC-RuYBfzYc3cHaLA");
   console.log(supabase);
-  const user=getUser();
+  const user=await getUser();
   const { data, error } = await supabase
     .from("game_proposition")
-    .update([gameData])
-    .eq("id",);
+    .update([{status:"accepted"}])
+    .eq("id",id);
   console.log(data);
   console.log(error);
   revalidatePath("/");
