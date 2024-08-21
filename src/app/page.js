701260a91bfa040e493af8400d12a0ec95c95ffd
BinahDaath <79816877+BinahDaath@users.noreply.github.com>
2024-08-21 07:14:45 +0000

    test

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,10 @@ import { MyLink } from "@/components/MyLink";
 export default function Home() {
   return (
     <div>
-      <MyLink href="/persons">Persons</MyLink>
+      <MyLink href="/choosegame">Choose Game</MyLink>
+      <MyLink href="/creategame">Create Game</MyLink>
+      <MyLink href="/mycreatedgame">My Created Game</MyLink>
+      <MyLink href="/myplayinggame">My Playing Game</MyLink>
     </div>
   );
 }
